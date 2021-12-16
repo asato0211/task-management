d1cedb5a9118f339dc8efaa53ef4eabfd7992785
asato0211 <mai0926yhm@gmail.com>
2021-12-17 01:20:38 +0900

    チェックボックスの挙動を修正

--- task-management/app/javascript/packs/components/index.vue
+++ task-management/app/javascript/packs/components/index.vue
@@ -1,15 +1,14 @@
-<template>
+ <template>
   <div>
     <!-- タスク新規作成 -->
-    <p><br></p>
     <div class="row margin-default">
       <div class="col s10 m11">
         <input v-model="newTask" id="new-task-form" class="form-control padding-default" placeholder="タスクを追加してください!">
       </div>
       <div class="col s2 m1">
-        <div v-on:click="createTask" class="btn-floating waves-effect waves-light red">
+        <button class="btn-floating waves-effect waves-light red" v-on:click="createTask">
           <i class="material-icons">add</i>
-        </div>
+        </button>
       </div>
     </div>
 
@@ -17,29 +16,31 @@
     <div>
       <ul class="collection">
         <li
+        v-bind:id="'row_task_' + task.id"
+        class="collection-item"
         v-for="task in filteredTasks"
-        :key="task.id"
-        class="collection-item">
-          <label v-bind:for="task.id" class="word-color-black">
-            <input type="checkbox" bind:key="task.id" v-on:change="doneTask(task.id)" />
-            <span>{{ task.title }}</span>
-          </label>
+        :key="task.id">
+        <label v-bind:for="'task_' + task.id" class="word-color-black">
+          <input type="checkbox" v-bind:id="'task_' + task.id" v-on:change="doneTask(task.id)" />
+          <span>{{ task.title }}</span>
+        </label>
         </li>
       </ul>
     </div>
 
     <!-- 完了済みタスクを表示するボタン -->
-    <div class="btn btn-custom" v-on:click="dispFinishedTasks">完了済みのタスクを表示</div>
+    <button class="btn btn-custom" v-on:click="displayFinishedTasks">完了済みのタスクを表示</button>
 
     <!-- 完了済みタスク一覧 -->
-    <div id="finished-tasks" class="disp_none">
+    <div id="finished-tasks" class="display_none">
       <ul class="collection">
         <li
+        v-bind:id="'row_task_' + task.id"
+        class="collection-item"
         v-for="task in filteredTasks"
-        v-bind:key="task.id"
-        class="collection-item">
-          <label v-bind:for="task.id" class="line-through">
-            <input type="checkbox" v-bind:key="task.id" checked="checked" />
+        :key="task.id">
+          <label v-bind:for="'task_' + task.id"  class="line-through">
+            <input type="checkbox" v-bind:id="'task_' + task.id" checked="checked"/>
             <span>{{ task.title }}</span>
           </label>
         </li>
@@ -47,7 +48,6 @@
     </div>
   </div>
 </template>
-
 
 <script>
   import axios from 'axios';
@@ -65,8 +65,15 @@
       this.fetchTasks();
     },
 
+    // 完了済みのタスクかどうか判定する
+    computed: {
+      filteredTasks() {
+        return this.tasks.filter(task => !task.done)
+      }
+    },
+
+    // APIで取得した値をtasksに格納する
     methods: {
-      // APIで取得した値をtasksに格納する
       fetchTasks() {
         axios.get('/api/tasks').then((response) => {
           for(let i = 0; i < response.data.tasks.length; i++) {
@@ -78,14 +85,9 @@
       },
 
       // 完了済みタスク一覧を表示する
-      dispFinishedTasks() {
-        document.querySelector('#finished-tasks').classList.toggle('disp_none')
-      },
-
-      // 新規タスクを追加
       createTask() {
         if (!this.newTask) return;
-        axios.post('/api/tasks' , { task: { title: this.newTask } }).then((response) => {
+        axios.post('/api/tasks', { task: { title: this.newTask } }).then((response) => {
           this.tasks.unshift(response.data.task);
           this.newTask = '';
         }, (error) => {
@@ -93,9 +95,8 @@
         });
       },
 
-      // タスク一覧→チェックボックスを押した際に動作する
+      // 新規タスクを追加
       doneTask(task_id) {
-        console.log('doneTask起動しました')
         axios.put('/api/tasks/' + task_id, { task: { done: 1 } }).then((response) => {
           this.moveFinishedTask(task_id);
         }, (error) => {
@@ -103,11 +104,16 @@
         });
       },
 
+      // タスク一覧→チェックボックスを押した際に動作する
+      displayFinishedTasks() {
+        document.querySelector('#finished-tasks').classList.toggle('display_none');
+      },
+
       // 完了済みタスク一覧に移動
-      moveFinishTask(task_id) {
-        const el = document.querySelector(task_id);
+      moveFinishedTask(task_id) {
+        const el = document.querySelector('#row_task_' + task_id);
         const el_clone = el.cloneNode(true);  // DOMをクローンしておく
-        el.classList.add('disp_none');  // 未完了のタスクを先に非表示にする
+        el.classList.add('display_none');     // 未完了のタスクを先に非表示にする
         // スタイルをあてて完了済みタスク一覧に追加する
         el_clone.getElementsByTagName('input')[0].checked = 'checked';
         el_clone.getElementsByTagName('label')[0].classList.add('line-through');
@@ -115,17 +121,9 @@
         const li = document.querySelector('#finished-tasks > ul > li:first-child');
         document.querySelector('#finished-tasks > ul').insertBefore(el_clone, li);
       }
-    },
-
-    // 完了済みのタスクかどうか判定する
-    computed: {
-      filteredTasks() {
-        return this.tasks.filter(task => !task.isDone)
-      }
     }
   }
 </script>
-
 
 <style scoped>
   [v-cloak] {
