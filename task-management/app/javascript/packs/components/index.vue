b314f591ecd53162de56e00551bf2449ff490667
asato0211 <mai0926yhm@gmail.com>
2021-12-20 00:01:52 +0900

    完了済み一覧でスタイルがあたらない箇所を修正

--- task-management/app/javascript/packs/components/index.vue
+++ task-management/app/javascript/packs/components/index.vue
@@ -1,4 +1,4 @@
- <template>
+<template>
   <div>
     <!-- タスク新規作成 -->
     <div class="row margin-default">
@@ -11,37 +11,41 @@
         </button>
       </div>
     </div>
-
+    
     <!-- タスク一覧を表示 -->
     <div>
       <ul class="collection">
         <li
         v-bind:id="'row_task_' + task.id"
         class="collection-item"
-        v-for="task in filteredTasks"
-        :key="task.id">
-        <label v-bind:for="'task_' + task.id" class="word-color-black">
-          <input type="checkbox" v-bind:id="'task_' + task.id" v-on:change="doneTask(task.id)" />
-          <span>{{ task.title }}</span>
-        </label>
+        v-for="task in tasks"
+        v-if="!task.done">
+          <label v-bind:for="'task_' + task.id">
+            <input type="checkbox" v-bind:id="'task_' + task.id" v-on:change="doneTask(task.id)" />
+            <span>
+              <label class="word-color-black">{{ task.title }}</label>
+            </span>
+          </label>
         </li>
       </ul>
     </div>
 
     <!-- 完了済みタスクを表示するボタン -->
     <button class="btn btn-custom" v-on:click="displayFinishedTasks">完了済みのタスクを表示</button>
-
+    
     <!-- 完了済みタスク一覧 -->
     <div id="finished-tasks" class="display_none">
       <ul class="collection">
         <li
         v-bind:id="'row_task_' + task.id"
         class="collection-item"
-        v-for="task in !filteredTasks"
-        :key="task.id">
-          <label v-bind:for="'task_' + task.id"  class="line-through">
+        v-for="task in tasks"
+        v-if="task.done">
+          <label v-bind:for="'task_' + task.id">
             <input type="checkbox" v-bind:id="'task_' + task.id" checked="checked"/>
-            <span>{{ task.title }}</span>
+            <span>
+              <label class="line-through">{{ task.title }}</label>
+            </span>
           </label>
         </li>
       </ul>
@@ -63,17 +67,10 @@
     // インスタンスがマウントされたタイミングで実行(ライフサイクルダイアグラム)
     mounted() {
       this.fetchTasks();
-    },
+    },   
 
-    // 完了済みのタスクかどうか判定する
-    computed: {
-      filteredTasks() {
-        return this.tasks.filter(task => !task.done)
-      }
-    },
-
-    // APIで取得した値をtasksに格納する
     methods: {
+      // APIで取得した値をtasksに格納する
       fetchTasks() {
         axios.get('/api/tasks').then((response) => {
           for(let i = 0; i < response.data.tasks.length; i++) {
@@ -84,7 +81,7 @@
         });
       },
 
-      // 完了済みタスク一覧を表示する
+      // 新規タスクを追加
       createTask() {
         if (!this.newTask) return;
         axios.post('/api/tasks', { task: { title: this.newTask } }).then((response) => {
@@ -95,9 +92,9 @@
         });
       },
 
-      // 新規タスクを追加
+      // 完了済みタスク一覧を表示する
       doneTask(task_id) {
-        axios.put('/api/tasks/' + task_id, { task: { done: 1 } }).then((response) => {
+        axios.put('/api/tasks/' + task_id, { task: { done: true } }).then((response) => {
           this.moveFinishedTask(task_id);
         }, (error) => {
           console.log(error);
@@ -112,6 +109,7 @@
       // 完了済みタスク一覧に移動
       moveFinishedTask(task_id) {
         const el = document.querySelector('#row_task_' + task_id);
+        // DOMをクローンしておく
         const el_clone = el.cloneNode(true);  // DOMをクローンしておく
         el.classList.add('display_none');     // 未完了のタスクを先に非表示にする
         // スタイルをあてて完了済みタスク一覧に追加する
