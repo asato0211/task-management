b7467dcf82fe883abdca05cd7689fdf8cefd3194
asato0211 <mai0926yhm@gmail.com>
2021-12-21 23:59:50 +0900

    モデルの単体テストを作成

--- task-management/test/models/task_test.rb
+++ task-management/test/models/task_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @task = Task.create(title: "test task")
+  end
+  
+  test "should be vaild" do
+    assert @task.valid?
+  end
+
+  test "title should be present" do
+    @task.title = " "
+    assert_not @task.valid?
+  end
+
+  # titleの最大文字数(70文字)のバリデーションをチェック
+  test "title should be at most 70 characters" do
+    @task.title = "a" * 71
+    assert_not @task.valid?
+  end
 end
