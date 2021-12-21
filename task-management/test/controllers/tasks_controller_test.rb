7a2dc49f61c21a14d2524f3acb25f142668c8edc
asato0211 <mai0926yhm@gmail.com>
2021-12-22 00:01:22 +0900

    機能テストを追加

--- task-management/test/controllers/tasks_controller_test.rb
+++ task-management/test/controllers/tasks_controller_test.rb
@@ -1,8 +1,15 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+
   test "should get index" do
-    get home_index_url
+    get root_url
+    assert_response :success
+    assert_select "title", "TaskManagement"
+  end
+
+  test "should get about" do
+    get about_url
     assert_response :success
   end
 end
