52e986eb7280fd2d8c11bbd3739e6e8d84f7aebe
asato0211 <mai0926yhm@gmail.com>
2022-01-10 21:25:49 +0900

    コントローラーのユニットテストを修正

--- task-management/test/controllers/tasks_controller_test.rb
+++ task-management/test/controllers/tasks_controller_test.rb
@@ -1,15 +1,33 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-
-  test "should get index" do
-    get root_url
-    assert_response :success
-    assert_select "title", "TaskManagement"
+  setup do
+    @task = tasks(:one)
   end
 
-  test "should get about" do
-    get about_url
+  test "indexアクションのレスポンステスト" do
+    get "/api/v1/tasks", as: :json
     assert_response :success
   end
+
+  test "createアクションのレスポンステスト" do
+    assert_difference('Task.count') do
+      post "/api/v1/tasks", params: { task: { title: @task.title, done: @task.done } }, as: :json
+    end
+
+    assert_response :created
+  end
+
+  test "updateアクションのレスポンステスト" do
+    patch "/api/v1/tasks/1", params: { task: { title: @task.title, done: @task.done } }, as: :json
+    assert_response :ok
+  end
+
+  test "destroyアクションのレスポンステスト" do
+    assert_difference('Task.count', -1) do
+      delete "/api/v1/tasks/1", as: :json
+    end
+
+    assert_response :ok
+  end  
 end
